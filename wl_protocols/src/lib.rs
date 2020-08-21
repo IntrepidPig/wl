a94cc88515182d91bde1eb24a3e7664c9903ad96
Benny Aguilera <bennycaguilera@gmail.com>
2020-08-21 13:38:21 -0700

    Move xdg_shell to wl_protocols

---
+++ wl_protocols/src/lib.rs
@@ -0,0 +1,20 @@
+macro_rules! define_protocol {
+	($name:ident, $path:expr) => {
+		pub mod $name {
+			mod private {
+				#[allow(unused)]
+				pub(in self) use wl_server::{
+					client::{ClientMap},
+					resource::{Resource, NewResource, Untyped},
+					protocol::*,
+				};
+
+				include!(concat!(env!("OUT_DIR"), $path));
+			}
+
+			pub use self::private::prelude::*;
+		}
+	};
+}
+
+define_protocol!(xdg_shell, "/xdg_shell_api.rs");
--- xdg_shell_protocol/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod xdg_shell {
-	pub mod private {
-		pub use wl_protocol::wl::*;
-		
-		include!(concat!(env!("OUT_DIR"), "/xdg_shell_api.rs"));
-	}
-
-	pub use private::prelude::*;
-}
